b30e5ed990f48bc113c2f1db8dca68a59f15c633
Jean Delize <jeanmarinodelize@gmail.com>
2021-06-19 17:19:20 -0300

    Criando repository e service para Categoria

--- src/main/java/com/jeandelize/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/jeandelize/cursomc/resources/CategoriaResource.java
@@ -1,29 +1,28 @@
 package com.jeandelize.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jeandelize.cursomc.domain.Categoria;
+import com.jeandelize.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
-		Categoria c1 = new Categoria(1,"Inforomática");
-		Categoria c2 = new Categoria(2,"Escritório");
-		
-		List<Categoria> lista = new ArrayList<>();
-		
-		lista.add(c1);
-		lista.add(c2);
-		
-		return lista;
+	@Autowired
+	private CategoriaService serviceCategoria;
+	
+	@RequestMapping(value="/{id}",method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Optional<Categoria> obj = serviceCategoria.buscar(id);
+		return ResponseEntity.ok().body(obj);
 	
 		
 	}
---
+++ src/main/java/com/jeandelize/cursomc/services/CategoriaService.java
@@ -0,0 +1,25 @@
+package com.jeandelize.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jeandelize.cursomc.domain.Categoria;
+import com.jeandelize.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Optional<Categoria> buscar(Integer id) {
+		
+		Optional<Categoria> obj = repo.findById(id);		
+	
+		return obj;
+	
+		
+	}
+}
