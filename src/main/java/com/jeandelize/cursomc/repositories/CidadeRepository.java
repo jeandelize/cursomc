918f5bd50207fc50fe82220cf56b13acfb806c36
Jean Delize <jeanmarinodelize@gmail.com>
2021-06-27 11:08:09 -0300

    Estado e cidade

---
+++ src/main/java/com/jeandelize/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,14 @@
+package com.jeandelize.cursomc.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jeandelize.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends  JpaRepository<Cidade, Integer>{
+
+
+}
