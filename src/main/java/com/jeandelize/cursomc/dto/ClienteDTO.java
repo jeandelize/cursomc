283df96e6689d9e58ff37e6445e16830b7e551c2
Jean Delize <jeanmarinodelize@gmail.com>
2022-04-23 12:33:10 -0300

    validacao customizada: email nao repetido na atualizacao de cliente

--- src/main/java/com/jeandelize/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/jeandelize/cursomc/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 import com.jeandelize.cursomc.domain.Cliente;
+import com.jeandelize.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO  implements Serializable {
 
 	private static final long serialVersionUID = 1L;
